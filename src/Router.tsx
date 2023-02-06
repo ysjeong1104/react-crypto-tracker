9cd3cd8e0b8072b12ba9746ebfbd41a486a60d9d
ysjeong1104 <whseok@icloud.com>
2023-02-06 22:31:55 +0900

    refresh 404 issue resolved

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import {HashRouter ,Routes,Route} from "react-router-dom";
+import {BrowserRouter ,Routes,Route} from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
 import Price from "./routes/Price";
@@ -12,7 +12,7 @@ const Router=()=>{
     
     
     return(
-        <HashRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>       
                 <Route path="/" element={<Coins/>} />         
                 <Route path="/:coinId" element={<Coin />} >
@@ -22,7 +22,7 @@ const Router=()=>{
                 </Route>
                 
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
 
